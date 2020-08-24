72fd00b1ac58343104612b0f22dab2fa0b42db24
Mikael <Miklosoft@gmail.com>
2020-08-24 17:16:16 +0300

    1. Disabled output to the console of service information.

--- src/main/java/task/parser/ParsertechnicaltaskApplication.java
+++ src/main/java/task/parser/ParsertechnicaltaskApplication.java
@@ -3,11 +3,17 @@ package task.parser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class ParsertechnicaltaskApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ParsertechnicaltaskApplication.class, args);
+        SpringApplication app = new SpringApplication(ParsertechnicaltaskApplication.class);
+        Properties properties = new Properties();
+        properties.setProperty("spring.main.banner-mode", "off");
+        properties.setProperty("logging.pattern.console", "");
+        app.setDefaultProperties(properties);
+        app.run(args);
     }
-
 }
